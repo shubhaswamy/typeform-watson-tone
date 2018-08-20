87d399190b65c45ce77da2e0b4328c16d222bcce
Shubha Swamy <shubhaswamy2@gmail.com>
2018-08-20 10:52:17 -0700

    added completed ibm action function code

--- assets/code/function.py
+++ assets/code/function.py
@@ -1,14 +1,6 @@
-#
-#
-# main() will be run when you invoke this action
-#
-# @param Cloud Functions actions accept a single parameter, which must be a JSON object.
-#
-# @return The output of this action, which must be a JSON object.
-#
-#
 
-# import libraries 
+# import libraries for watson & to work with JSON data 
+import requests
 import sys
 import json
 from watson_developer_cloud import ToneAnalyzerV3
@@ -22,16 +14,16 @@ def main(args):
     # parse through typeform response payload 
     for x in response["answers"]:
         answersID = x['field']['id']
-        questionID = 'R2xIdf96H85x'
+        questionID = args.get('questionID') #get custom questionID from parameters
         
         if (answersID == questionID):
             answerText = x["text"]
     
-    #set username and password
+    #set username and password from parameters
     tone_analyzer = ToneAnalyzerV3(
     version ='2017-09-21',
-    username ='{username}', # replace this
-    password ='{password}' # replace this 
+    username = args.get('username'),
+    password =args.get('password') 
     )
     
     content_type = 'application/json'
@@ -39,5 +31,65 @@ def main(args):
     #return tone in JSON format
     tone = tone_analyzer.tone({"text": answerText},content_type) 
     
-    # print(json.dumps(tone, indent=2))
+    
+    
+    
+    # ----- send data to slack -----
+    
+    #parse through data to send in slack
+    tones = str(tone['document_tone']['tones'])
+    review = ""
+    for x in tone['sentences_tone']:
+        review += (x["text"] + " ")
+
+    allTones = ""
+    for y in tone['document_tone']['tones']:
+        allTones += (y['tone_name'] + " ")
+        
+    scoreTotal = 0
+    count = 0
+    for z in tone['document_tone']['tones']:
+        scoreTotal += z["score"]
+        count += 1
+
+    averageScore = scoreTotal/count
+    strAvgScore = str(averageScore)
+    
+
+    # send slack payload 
+    slack_data = {
+    "attachments": [
+        {
+            "pretext": "New review submitted",
+            "color": "#0b439e",
+            "text": review,
+            "fields": [
+                {
+                    "title": "Tones Found in Review",
+                    "value": allTones,
+                    "short": False
+                },
+                {
+                    "title": "Average Tone Score",
+                    "value": strAvgScore,
+                    "short": False
+                }
+            ]
+        }
+    ]
+}
+
+    # get custom webhook from parameter 
+    webhook_url = args.get('slackWebhook')
+    
+
+    #Post data to slack
+    response = requests.post(
+        webhook_url, data=json.dumps(slack_data),
+        headers={'Content-Type': 'application/json'}
+    )
+    
+    #return JSON format   
+    return {"body":tone}
+    
     
